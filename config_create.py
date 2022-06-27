a30d8993aab8898cc4d20322373cdd89a832a44a
teretupiro <vafin_2005@list.ru>
2022-06-27 11:00:08 +0300

    robot_2

--- config_create.py
+++ config_create.py
@@ -1,6 +1,7 @@
 
 import json,os
-
+from module_read import *
+from robot import file
 
 listfiles=os.listdir(path='students')
 dirs=[d for d in os.listdir('students')
@@ -8,8 +9,11 @@ dirs=[d for d in os.listdir('students')
 
 students_dict={}
 for i in range(len(dirs)):
+  students_dict.update([(dirs[i],data)])
 
-  students_dict.update([(dirs[i],'empty')])
+f=open("config_2.txt","w+")
+f.write(json.dumps(students_dict))
+f.close
 
 
 print(students_dict)
